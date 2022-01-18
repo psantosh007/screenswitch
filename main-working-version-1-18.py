9ecae3af493b8e864036cae67ebc3063bb1f182f
psantosh007 <97275378+psantosh007@users.noreply.github.com>
2022-01-18 22:39:06 +0400

    Add files via upload
    The screen manager works - Next steps are to add button in proper size and location ; it's learnt that complicated children tend to crash the .apk.

---
+++ main-working-version-1-18.py
@@ -0,0 +1,72 @@
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_string("""
+<CusButton@Button>:
+    font_size: 40
+    
+<CusText@TextInput>:
+    font_size: 35
+    multiline: False
+    
+<Screen1>:
+    name: 'screen1'
+    canvas.before:
+        Color:
+            rgba: (1, 1, 1, 1)
+        Rectangle:
+            source:'back.jpg'
+            size: root.width, root.height
+            pos: self.pos
+
+    BoxLayout:
+        rows: 1
+        pos_hint: {"top": 1,"left": 1}
+        size_hint: 1, .1
+        CusButton:
+            text: "Next"
+            on_press: root.manager.current = 'screen2'
+                              
+<Screen2>:
+    name: 'screen2'
+    BoxLayout:
+        CusButton:
+            text: "Previous"
+            on_press: root.manager.current = 'screen1'
+
+""")
+
+#class twonumgrid(GridLayout):
+class Screen1(Screen):
+    pass
+#    label_wid = ObjectProperty()
+#    butt1 = ObjectProperty()
+#    info = StringProperty()
+    
+
+#    def connect_mqtt(self):
+#       self.label_wid.text= "Hello start conn"
+        
+    
+#    def disconnect_mqtt(self):    
+#        self.label_wid.text= "Disconnected"
+        
+class Screen2(Screen):
+    pass
+            
+sm= ScreenManager()
+sm.add_widget(Screen1(name='screen1'))
+sm.add_widget(Screen2(name='screen2'))
+
+      
+class TestApp(App):
+
+    def build(self):
+        return sm
+         
+if __name__ == "__main__":
+    TestApp().run()
