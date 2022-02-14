a519f0c56d647fadedcdd39df15c13270b9a3a5b
psantosh007 <97275378+psantosh007@users.noreply.github.com>
2022-02-14 14:56:19 +0400

    Add files via upload

---
+++ main-version-14-02-afternoon.py
@@ -0,0 +1,536 @@
+import kivy
+import paho.mqtt.client as mqtt
+import time
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+import threading
+import socket
+from pythonping import ping
+
+# For mobile phone
+from android.storage import primary_external_storage_path
+primary_ext_storage = primary_external_storage_path()
+
+from android.permissions import request_permissions, Permission
+request_permissions([Permission.INTERNET])
+
+from android.permissions import request_permissions, Permission
+request_permissions([Permission.ACCESS_NETWORK_STATE])
+
+import certifi
+import os
+os.environ['SSL_CERT_FILE'] = certifi.where()
+
+
+
+Builder.load_string("""
+<CusButton@Button>:
+    font_size: 40
+    
+<CusText@TextInput>:
+    font_size: 35
+    multiline: False
+    
+<Screen1>:
+    name: 'screen1'
+    subscription_data: subscription_text
+    conn_data: conn_text
+    canvas.before:
+        Color:
+            rgba: (1, 1, 1, 1)
+        Rectangle:
+            source:'back.jpg'
+            size: root.width, root.height
+            pos: self.pos
+
+    BoxLayout:
+        rows: 3
+        
+        orientation: "vertical"
+        pos_hint: {"top": 1,"left": 1}
+        size_hint: 1, .5
+        CusButton:
+            text: "Next"
+            on_press: root.manager.current = 'screen2'
+        CusButton:
+            text: "SUBSCRIBE"
+            on_press: root.mqtt_subscribe()
+        CusButton:
+            id: conn_text
+            text: "PRESS"
+            on_press: root.press()
+            
+        CusText:
+            id: server_id
+            hint_text: "Write Server name"
+        CusText:
+            id: subscription_id
+            hint_text: "Write Subscrption ID"
+        Label:
+            id: subscription_text
+            text: "GeeksForGeeks \\n is the best platform for DSA content"
+        
+
+            
+        
+                              
+<Screen2>:
+    name: 'screen2'
+    canvas.before:
+        Color:
+            rgba: (1, 1, 1, 1)
+        Rectangle:
+            source:'back.jpg'
+            size: root.width, root.height
+            pos: self.pos
+    BoxLayout:
+        CusButton:
+            text: "Previous"
+            pos_hint: {"top": 0.1, "left": 0.5}
+            size_hint: .1, .1 
+            on_press: root.manager.current = 'screen1'
+
+""")
+
+#class twonumgrid(GridLayout):
+class Screen1(Screen):
+    conn_data = ObjectProperty()
+    subscription_data = ObjectProperty()
+ # this is a test
+
+    def is_connected():
+        try:
+        # connect to the host -- tells us if the host is actually
+        # reachable
+            socket.create_connection(("1.1.1.1", 53))
+            return True
+        except OSError:
+            pass
+        return False
+
+    def mqtt_subscribe(self):
+        
+        a = "ff"  
+        b="cc"  
+        c="ff"
+        sam=[a,b,c]
+
+        sam_comb= "\n".join(sam)
+        
+        self.subscription_data.text= sam_comb
+        time.sleep(1)
+        # checking internet connection
+#        sss=self.is_connected()
+        sss=socket.gethostbyname('www.google.com')
+        self.subscription_data.text= str(sss)
+
+
+    
+    def press(self):
+        self.subscription_data.text= "Press"
+        threading.Thread(target=self.update_text).start()
+    #    client = mqtt.Client("P1")
+    #    self.client.connect("test.mosquitto.org",1883)
+
+    def on_connect(self, client, userdata, flags, rc):
+        if (rc == 0):
+            self.subscription_data.text= "Connected"
+        else:
+            self.subscription_data.text= "Not connected"
+
+    def update_text(self):
+        self.subscription_data.text= "Press-update text"
+        client = mqtt.Client("P1", protocol=mqtt.MQTTv31)
+        self.subscription_data.text= "Press-update text2"
+        #self.client.on_connect = self.on_connect
+        #self.client.connect("test.mosquitto.org",port=1883)
+#        for x in range(10):
+#            a=self.client.connect("192.168.1.20", 1883, 60 ) 
+#            time.sleep(0.1)
+#            self.subscription_data.text = "ss"+str(a)
+        #self.client.on_connect= self.on_connect
+        # pinging the host broker
+        result=ping('test.mosquitto.org', count=1)
+        if result.success():
+            self.subscription_data.text = "True"
+        else:
+            self.subscription_data.text = "False"
+        time.sleep(1)
+        self.subscription_data.text= "Press-update text3"
+
+
+        
+class Screen2(Screen):
+    pass
+            
+sm= ScreenManager()
+sm.add_widget(Screen1(name='screen1'))
+sm.add_widget(Screen2(name='screen2'))
+
+      
+class TestApp(App):
+    
+
+    def build(self):
+#        self.client.on_message=self.on_message 
+        return sm
+#    def on_start(self):
+#        client = mqtt.Client("P1")
+#        self.client.connect("test.mosquitto.org",1883)
+ #       self.client.loop_start()
+ #       self.client.subscribe("santosh/mars")
+
+
+
+
+if __name__ == "__main__":
+    TestApp().run()
+
+
+
+
+
+[app]
+
+# (str) Title of your application
+title = My Application
+
+# (str) Package name
+package.name = myapp
+
+# (str) Package domain (needed for android/ios packaging)
+package.domain = org.test
+
+# (str) Source code where the main.py live
+source.dir = .
+
+# (list) Source files to include (let empty to include all the files)
+source.include_exts = py,png,jpg,kv,atlas
+
+# (list) List of inclusions using pattern matching
+#source.include_patterns = assets/*,images/*.png
+
+# (list) Source files to exclude (let empty to not exclude anything)
+#source.exclude_exts = spec
+
+# (list) List of directory to exclude (let empty to not exclude anything)
+#source.exclude_dirs = tests, bin
+
+# (list) List of exclusions using pattern matching
+#source.exclude_patterns = license,images/*/*.jpg
+
+# (str) Application versioning (method 1)
+version = 0.1
+
+# (str) Application versioning (method 2)
+# version.regex = __version__ = ['"](.*)['"]
+# version.filename = %(source.dir)s/main.py
+
+# (list) Application requirements
+# comma separated e.g. requirements = sqlite3,kivy
+requirements = python3,kivy==2.0.0rc1,paho-mqtt,python-socketio,pythonping,android, openssl, pyopenssl, httplib2,requests,certifi,urllib3,chardet,idna,plyer,kivy_garden.mapview, pytz
+
+# (str) Custom source folders for requirements
+# Sets custom source for any requirements with recipes
+# requirements.source.kivy = ../../kivy
+
+# (list) Garden requirements
+#garden_requirements =
+
+# (str) Presplash of the application
+#presplash.filename = %(source.dir)s/data/presplash.png
+
+# (str) Icon of the application
+#icon.filename = %(source.dir)s/data/icon.png
+
+# (str) Supported orientation (one of landscape, sensorLandscape, portrait or all)
+orientation = portrait
+
+# (list) List of service to declare
+#services = NAME:ENTRYPOINT_TO_PY,NAME2:ENTRYPOINT2_TO_PY
+
+#
+# OSX Specific
+#
+
+#
+# author = © Copyright Info
+
+# change the major version of python used by the app
+osx.python_version = 3
+
+# Kivy version to use
+osx.kivy_version = 1.9.1
+
+#
+# Android specific
+#
+
+# (bool) Indicate if the application should be fullscreen or not
+fullscreen = 0
+
+# (string) Presplash background color (for new android toolchain)
+# Supported formats are: #RRGGBB #AARRGGBB or one of the following names:
+# red, blue, green, black, white, gray, cyan, magenta, yellow, lightgray,
+# darkgray, grey, lightgrey, darkgrey, aqua, fuchsia, lime, maroon, navy,
+# olive, purple, silver, teal.
+#android.presplash_color = #FFFFFF
+
+# (list) Permissions
+android.permissions = INTERNET, ACCESS_NETWORK_STATE, ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE
+
+# (int) Target Android API, should be as high as possible.
+android.api = 29
+
+# (int) Minimum API your APK will support.
+#android.minapi = 21
+
+# (int) Android SDK version to use
+#android.sdk = 20
+
+# (str) Android NDK version to use
+#android.ndk = 19b
+
+# (int) Android NDK API to use. This is the minimum API your app will support, it should usually match android.minapi.
+#android.ndk_api = 21
+
+# (bool) Use --private data storage (True) or --dir public storage (False)
+#android.private_storage = True
+
+# (str) Android NDK directory (if empty, it will be automatically downloaded.)
+#android.ndk_path =
+
+# (str) Android SDK directory (if empty, it will be automatically downloaded.)
+#android.sdk_path =
+
+# (str) ANT directory (if empty, it will be automatically downloaded.)
+#android.ant_path =
+
+# (bool) If True, then skip trying to update the Android sdk
+# This can be useful to avoid excess Internet downloads or save time
+# when an update is due and you just want to test/build your package
+# android.skip_update = False
+
+# (bool) If True, then automatically accept SDK license
+# agreements. This is intended for automation only. If set to False,
+# the default, you will be shown the license when first running
+# buildozer.
+# android.accept_sdk_license = False
+
+# (str) Android entry point, default is ok for Kivy-based app
+#android.entrypoint = org.renpy.android.PythonActivity
+
+# (str) Android app theme, default is ok for Kivy-based app
+# android.apptheme = "@android:style/Theme.NoTitleBar"
+
+# (list) Pattern to whitelist for the whole project
+#android.whitelist =
+
+# (str) Path to a custom whitelist file
+#android.whitelist_src =
+
+# (str) Path to a custom blacklist file
+#android.blacklist_src =
+
+# (list) List of Java .jar files to add to the libs so that pyjnius can access
+# their classes. Don't add jars that you do not need, since extra jars can slow
+# down the build process. Allows wildcards matching, for example:
+# OUYA-ODK/libs/*.jar
+#android.add_jars = foo.jar,bar.jar,path/to/more/*.jar
+
+# (list) List of Java files to add to the android project (can be java or a
+# directory containing the files)
+#android.add_src =
+
+# (list) Android AAR archives to add (currently works only with sdl2_gradle
+# bootstrap)
+#android.add_aars =
+
+# (list) Gradle dependencies to add (currently works only with sdl2_gradle
+# bootstrap)
+#android.gradle_dependencies =
+
+# (list) add java compile options
+# this can for example be necessary when importing certain java libraries using the 'android.gradle_dependencies' option
+# see https://developer.android.com/studio/write/java8-support for further information
+# android.add_compile_options = "sourceCompatibility = 1.8", "targetCompatibility = 1.8"
+
+# (list) Gradle repositories to add {can be necessary for some android.gradle_dependencies}
+# please enclose in double quotes 
+# e.g. android.gradle_repositories = "maven { url 'https://kotlin.bintray.com/ktor' }"
+#android.add_gradle_repositories =
+
+# (list) packaging options to add 
+# see https://google.github.io/android-gradle-dsl/current/com.android.build.gradle.internal.dsl.PackagingOptions.html
+# can be necessary to solve conflicts in gradle_dependencies
+# please enclose in double quotes 
+# e.g. android.add_packaging_options = "exclude 'META-INF/common.kotlin_module'", "exclude 'META-INF/*.kotlin_module'"
+#android.add_gradle_repositories =
+
+# (list) Java classes to add as activities to the manifest.
+#android.add_activities = com.example.ExampleActivity
+
+# (str) OUYA Console category. Should be one of GAME or APP
+# If you leave this blank, OUYA support will not be enabled
+#android.ouya.category = GAME
+
+# (str) Filename of OUYA Console icon. It must be a 732x412 png image.
+#android.ouya.icon.filename = %(source.dir)s/data/ouya_icon.png
+
+# (str) XML file to include as an intent filters in <activity> tag
+#android.manifest.intent_filters =
+
+# (str) launchMode to set for the main activity
+#android.manifest.launch_mode = standard
+
+# (list) Android additional libraries to copy into libs/armeabi
+#android.add_libs_armeabi = libs/android/*.so
+#android.add_libs_armeabi_v7a = libs/android-v7/*.so
+#android.add_libs_arm64_v8a = libs/android-v8/*.so
+#android.add_libs_x86 = libs/android-x86/*.so
+#android.add_libs_mips = libs/android-mips/*.so
+
+# (bool) Indicate whether the screen should stay on
+# Don't forget to add the WAKE_LOCK permission if you set this to True
+#android.wakelock = False
+
+# (list) Android application meta-data to set (key=value format)
+#android.meta_data =
+
+# (list) Android library project to add (will be added in the
+# project.properties automatically.)
+#android.library_references =
+
+# (list) Android shared libraries which will be added to AndroidManifest.xml using <uses-library> tag
+#android.uses_library =
+
+# (str) Android logcat filters to use
+android.logcat_filters = *:S python:D
+
+# (bool) Copy library instead of making a libpymodules.so
+#android.copy_libs = 1
+
+# (str) The Android arch to build for, choices: armeabi-v7a, arm64-v8a, x86, x86_64
+android.arch = armeabi-v7a
+
+# (int) overrides automatic versionCode computation (used in build.gradle)
+# this is not the same as app version and should only be edited if you know what you're doing
+# android.numeric_version = 1
+
+#
+# Python for android (p4a) specific
+#
+
+# (str) python-for-android fork to use, defaults to upstream (kivy)
+#p4a.fork = kivy
+
+# (str) python-for-android branch to use, defaults to master
+#p4a.branch = master
+
+# (str) python-for-android git clone directory (if empty, it will be automatically cloned from github)
+#p4a.source_dir =
+
+# (str) The directory in which python-for-android should look for your own build recipes (if any)
+#p4a.local_recipes =
+
+# (str) Filename to the hook for p4a
+#p4a.hook =
+
+# (str) Bootstrap to use for android builds
+# p4a.bootstrap = sdl2
+
+# (int) port number to specify an explicit --port= p4a argument (eg for bootstrap flask)
+#p4a.port =
+
+
+#
+# iOS specific
+#
+
+# (str) Path to a custom kivy-ios folder
+#ios.kivy_ios_dir = ../kivy-ios
+# Alternately, specify the URL and branch of a git checkout:
+ios.kivy_ios_url = https://github.com/kivy/kivy-ios
+ios.kivy_ios_branch = master
+
+# Another platform dependency: ios-deploy
+# Uncomment to use a custom checkout
+#ios.ios_deploy_dir = ../ios_deploy
+# Or specify URL and branch
+ios.ios_deploy_url = https://github.com/phonegap/ios-deploy
+ios.ios_deploy_branch = 1.7.0
+
+# (str) Name of the certificate to use for signing the debug version
+# Get a list of available identities: buildozer ios list_identities
+#ios.codesign.debug = "iPhone Developer: <lastname> <firstname> (<hexstring>)"
+
+# (str) Name of the certificate to use for signing the release version
+#ios.codesign.release = %(ios.codesign.debug)s
+
+
+[buildozer]
+
+# (int) Log level (0 = error only, 1 = info, 2 = debug (with command output))
+log_level = 2
+
+# (int) Display warning if buildozer is run as root (0 = False, 1 = True)
+warn_on_root = 1
+
+# (str) Path to build artifact storage, absolute or relative to spec file
+# build_dir = ./.buildozer
+
+# (str) Path to build output (i.e. .apk, .ipa) storage
+# bin_dir = ./bin
+
+#    -----------------------------------------------------------------------------
+#    List as sections
+#
+#    You can define all the "list" as [section:key].
+#    Each line will be considered as a option to the list.
+#    Let's take [app] / source.exclude_patterns.
+#    Instead of doing:
+#
+#[app]
+#source.exclude_patterns = license,data/audio/*.wav,data/images/original/*
+#
+#    This can be translated into:
+#
+#[app:source.exclude_patterns]
+#license
+#data/audio/*.wav
+#data/images/original/*
+#
+
+
+#    -----------------------------------------------------------------------------
+#    Profiles
+#
+#    You can extend section / key with a profile
+#    For example, you want to deploy a demo version of your application without
+#    HD content. You could first change the title to add "(demo)" in the name
+#    and extend the excluded directories to remove the HD content.
+#
+#[app@demo]
+#title = My Application (demo)
+#
+#[app:source.exclude_patterns@demo]
+#images/hd/*
+#
+#    Then, invoke the command line with the "demo" profile:
+#
+#buildozer --profile demo android debug
+
+
+
+
+
+
+
+
+
+
+
+
